5d9b89a0d19cdac325e788cf284dc0321459c97a
Juan Duque <sebastian@byafro.com>
2017-08-02 19:22:20 -0400

    bootstrap and pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'pages/home'
+  root 'pages#home'
+  get 'login' => 'pages#login'
+  get 'sign-up' => 'pages#sign-up'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
